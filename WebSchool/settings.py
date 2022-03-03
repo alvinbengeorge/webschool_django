e03b0d0920e30f79fc628ca67b02588151215a37
Shreyas <46835608+shreyas44@users.noreply.github.com>
2022-03-04 02:40:54 +0530

    Update migrations and settings for mysql

--- WebSchool/settings.py
+++ WebSchool/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'WebSchool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webschool_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'database',
+        'PORT': '3306',
     }
 }
 
